43132a83c28317b7c025c51dfbc819e99f86aff6
Alen <info@alenbarac.com>
2023-11-20 21:00:02 +0100

    button and row as a reusable styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import GlobalStyles from './styles/GlobalStyles'
+import Button from './ui/Button'
 import Heading from './ui/Heading'
+import Row from './ui/Row'
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Heading type="h1">Hello world</Heading>
-      <Heading as="h2">Hello world</Heading>
-      <Heading as="h3">Hello world</Heading>
+      <Row type="vertical">
+        <Heading type="h1">Hello world</Heading>
+      </Row>
+      <div>
+        <Heading as="h2">Hello world</Heading>
+      </div>
+      <Row type="vertical">
+        <Heading as="h3">Hello world</Heading>
+        <Heading as="h3">Hello world</Heading>
+      </Row>
+
+      <Row type="horizontal">
+        <Button variation="primary" size="medium">
+          Check in
+        </Button>
+
+        <Button variation="danger" size="small">
+          Kick out
+        </Button>
+
+        <Button>Check out</Button>
+      </Row>
     </>
   )
 }
