9a92dbd5f9a9b72200e5f9779a9047b3963e9705
Alen <info@alenbarac.com>
2023-11-21 19:35:11 +0100

    app main layout completed

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Account from './pages/Account'
 import Cabins from './pages/Cabins'
 import Login from './pages/Login'
 import Users from './pages/Users'
+import AppLayout from './ui/AppLayout'
 
 function App() {
   return (
@@ -16,13 +17,16 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
