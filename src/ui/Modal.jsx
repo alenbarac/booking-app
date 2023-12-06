4a3709e7efa86b21c2ee859ca4da957ad0c6cf3b
Alen <info@alenbarac.com>
2023-12-06 22:41:24 +0100

    modal close on outside click added

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,5 +1,12 @@
-import { cloneElement, createElement, useContext, useState } from 'react'
-import { createContext } from 'react'
+import {
+  cloneElement,
+  createContext,
+  createElement,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
 import { createPortal } from 'react-dom'
 import { HiXMark } from 'react-icons/hi2'
 import styled from 'styled-components'
@@ -83,11 +90,26 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, closeModal } = useContext(ModalContext)
+  const ref = useRef()
+
+  useEffect(() => {
+    function handleClick(e) {
+      if (ref.current && !ref.current.contains(e.target)) {
+        closeModal()
+      }
+    }
+    document.addEventListener('click', handleClick, true)
+
+    return () => {
+      document.removeEventListener('click', handleClick, true)
+    }
+  }, [closeModal])
+
   if (name !== openName) return null
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={closeModal}>
           <HiXMark />
         </Button>
